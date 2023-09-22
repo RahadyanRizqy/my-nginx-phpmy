19a17fe4c6f2aa447b46d199986f3b629e10f08e
Rahadyan Rizqy <rahadyanrizqy@gmail.com>
2023-09-22 21:58:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -22,7 +22,8 @@ cp phpmyadmin/config.sample.inc.php phpmyadmin/config.inc.php
 sed -i "s/\$cfg\['blowfish_secret'\] = '.*';/\$cfg['blowfish_secret'] = 'JBz?DX]#m\$Vy[m+M}o9jo?iMzpnQ9|U-';/" phpmyadmin/config.inc.php
 mv phpmyadmin /var/www/html/phpmyadmin
 chmod 777 /var/www/html/phpmyadmin
-chmod 777 /var/www/html/
+chmod 777 /var/www/html
+service nginx restart
 
 mysql_secure_installation
 
